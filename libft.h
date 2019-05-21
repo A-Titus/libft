491869a5b753c90c8c74efea7d1a47252e78613a
Abdussamad TITUS <atitus@c4r10s2.wethinkcode.co.za>
2019-05-21 15:26:32 +0200

    updating libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: atitus <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 09:17:20 by atitus            #+#    #+#             */
-/*   Updated: 2019/05/21 11:12:57 by atitus           ###   ########.fr       */
+/*   Updated: 2019/05/21 14:40:53 by atitus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,8 @@ int			ft_isprint(int a);
 int			ft_isascii(int a);
 int			ft_isalnum(int a);
 int			ft_isdigit(int a);
+int			ft_isalpha(int a);
+int			ft_atoi(char *str);
+int			ft_strcmp(const char *s1, const char *s2);
 
 #endif
