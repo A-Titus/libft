a3dde33d0b0946afac86c67ee3dacf8dbdd08f2b
Abdussamad TITUS <atitus@c4r10s2.wethinkcode.co.za>
2019-05-21 11:23:38 +0200

    update

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: atitus <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 09:17:20 by atitus            #+#    #+#             */
-/*   Updated: 2019/05/21 08:52:16 by atitus           ###   ########.fr       */
+/*   Updated: 2019/05/21 11:12:57 by atitus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ void		ft_putstr(char const *s);
 int			ft_tolower(int c);
 int			ft_toupper(int c);
 int			ft_isprint(int a);
-
+int			ft_isascii(int a);
+int			ft_isalnum(int a);
+int			ft_isdigit(int a);
 
 #endif
