147c68166a518623a08db5290b39577d75fb5a98
Abdussamad TITUS <atitus@c4r10s2.wethinkcode.co.za>
2019-05-22 15:29:20 +0200

    libft.h

--- libft.h
+++ libft.h
@@ -6,11 +6,14 @@
 /*   By: atitus <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 09:17:20 by atitus            #+#    #+#             */
-/*   Updated: 2019/05/21 14:40:53 by atitus           ###   ########.fr       */
+/*   Updated: 2019/05/22 14:44:31 by atitus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 #ifndef libft
 #define libft
@@ -26,5 +29,12 @@ int			ft_isdigit(int a);
 int			ft_isalpha(int a);
 int			ft_atoi(char *str);
 int			ft_strcmp(const char *s1, const char *s2);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
+size_t		ft_strlen(const char *str);
+char		*ft_strcpy(char *dest, const char *src);
+char		*ft_strncpy(char *dest, const char *src, size_t n);
+char		*ft_strdup(const char *s);
+char		*ft_strcat(char *dest, const char *src);
+char		*ft_strncat(char *dest, const char *src, size_t n);
 
 #endif
