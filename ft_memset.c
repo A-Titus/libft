9e401713a7e99baf06284a39c97bded3b5996026
Abdussamad TITUS <atitus@c4r10s2.wethinkcode.co.za>
2019-05-24 10:25:26 +0200

    ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: atitus <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 10:16:36 by atitus            #+#    #+#             */
-/*   Updated: 2019/05/24 10:16:40 by atitus           ###   ########.fr       */
+/*   Updated: 2019/05/24 10:24:56 by atitus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,16 @@
 void	*ft_memset(void *str, int c, size_t n)
 {
 	unsigned char	*dst;
+	unsigned int	i;
 
+	i = 0;
 	if (n == 0)
 		return (str);
-	dst = (unsigned char *)str;
-	while (n--)
+	dst = str;
+	while (i < n)
 	{
-		*dst = (unsigned char)c;
-		if (n)
-			dst++;
+		dst[i] = (unsigned char)c;
+		i++;
 	}
 	return (str);
 }
