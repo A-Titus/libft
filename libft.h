e29367bc57a21b0fc418474de884226ac2646c01
Abdussamad TITUS <atitus@c4r10s2.wethinkcode.co.za>
2019-05-24 11:50:21 +0200

    libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: atitus <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 09:17:20 by atitus            #+#    #+#             */
-/*   Updated: 2019/05/22 14:44:31 by atitus           ###   ########.fr       */
+/*   Updated: 2019/05/24 11:17:38 by atitus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,5 +36,8 @@ char		*ft_strncpy(char *dest, const char *src, size_t n);
 char		*ft_strdup(const char *s);
 char		*ft_strcat(char *dest, const char *src);
 char		*ft_strncat(char *dest, const char *src, size_t n);
+void		*ft_memset(void *str, int c, size_t n);
+void		ft_bzero(void *s, size_t n);
+void		*ft_memcpy(void *str1, const void *str2, size_t n);
 
 #endif
