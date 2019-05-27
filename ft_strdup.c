b224123e2500d34230dc27d432e2eabe25f94b73
Abdussamad TITUS <atitus@c4r10s2.wethinkcode.co.za>
2019-05-27 15:00:33 +0200

    ft_strdup.c

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: atitus <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 12:00:22 by atitus            #+#    #+#             */
-/*   Updated: 2019/05/22 13:10:01 by atitus           ###   ########.fr       */
+/*   Updated: 2019/05/27 12:39:28 by atitus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,19 @@
 
 char	*ft_strdup(const char *s)
 {
-	int		i;
-	int		len;
-	char	*str;
-
-	len = 0;
-	while (s[len] != '\0')
-	{
-		len++;
-	}
-	str = (char*)malloc(sizeof(*str) * (len + 1));
+	char *str;
+	int i;
+	
 	i = 0;
-	while (i < len)
+	str = (char*)malloc(sizeof(*str) * ft_strlen(s) + 1);
+	if (str == NULL)
+		return (NULL);
+
+	while (s[i])
 	{
 		str[i] = s[i];
 		i++;
 	}
-	free(str);
+	str[i] = '\0';
 	return (str);
 }
