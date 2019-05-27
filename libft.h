5ccccb71f2697b052754847d7b28a6be5ac6610b
root <root@LAPTOP-L53D8KQ9.localdomain>
2019-05-27 20:36:54 +0200

    libft.h

--- libft.h
+++ libft.h
@@ -40,7 +40,7 @@ void		*ft_memset(void *str, int c, size_t n);
 void		ft_bzero(void *s, size_t n);
 void		*ft_memcpy(void *str1, const void *str2, size_t n);
 void		*ft_memccpy(void *dest, const void *src, int c, size_t n);
-int			ft_memcmp(const void *str1, const void *str2, size_t n);
+int     	ft_memcmp(const void *s1, const void *s2, size_t n);
 void 		*ft_memchr(const void *str, int c, size_t n);
 void		*ft_memmove(void *str1, const void *str2, size_t n);
 char    	*ft_strstr(const char *haystack, const char *needle);
