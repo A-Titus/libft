1b9fe8c6345ce52e1dbd95320ecf1a8a79b44213
Abdussamad TITUS <atitus@c4r10s2.wethinkcode.co.za>
2019-05-30 12:34:16 +0200

    ft_strmap.c

---
+++ ft_strmap.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atitus <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/30 12:09:15 by atitus            #+#    #+#             */
+/*   Updated: 2019/05/30 12:31:32 by atitus           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmap(char const *s, char (*f)(char))
+{
+	char	*new;
+	int i;
+
+	i = 0;
+	new = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (new == NULL)
+		return (NULL);
+	while(s[i])
+	{
+		new[i] = (*f)(s[i]);
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
+}
