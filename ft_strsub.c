ca9fc8e9137a55e117b0ecc2ee927de1458d6bfc
Abdussamad TITUS <atitus@c4r10s2.wethinkcode.co.za>
2019-05-31 11:14:43 +0200

    libft unit test passed ,but gives seg fault with main

---
+++ ft_strsub.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atitus <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/31 10:42:49 by atitus            #+#    #+#             */
+/*   Updated: 2019/05/31 11:11:51 by atitus           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char		*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char *str;
+	size_t i;
+
+	i = 0;
+	if (s == NULL)
+		return (NULL);
+	if (start > ft_strlen(s))
+		return (NULL);
+	if((str = (char *)malloc(sizeof(char) * (len + 1))) == NULL)
+		return(NULL);
+	while(s[start] && i < len)
+	{
+		str[i] = s[start];
+		i++;
+		start++;
+	}
+	str[i] = '\0';
+	return(str);
+
+}
