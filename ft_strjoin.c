ced30a1cc7e1d2a9ab090f0f817856c13497a61f
root <root@LAPTOP-L53D8KQ9.localdomain>
2019-06-02 20:02:04 +0200

    ft_strjoin.c

---
+++ ft_strjoin.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const*s2)
+{
+	int len;
+	char *str;
+
+	len = ft_strlen(s1) + ft_strlen(s2);
+	str = (char *)malloc(sizeof(*str)*(len + 1));
+	if (str==NULL)
+		return(NULL);
+	ft_strcpy(str, s1);
+	ft_strcat(str,s2);
+	return(str);
+
+}
+
