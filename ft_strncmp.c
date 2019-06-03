83cecc2262b043327ed6d256bc9dbf0d3bd3438c
Abdussamad TITUS <atitus@c4r10s2.wethinkcode.co.za>
2019-06-03 14:45:11 +0200

    ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: atitus <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 07:50:36 by atitus            #+#    #+#             */
-/*   Updated: 2019/05/27 12:00:16 by atitus           ###   ########.fr       */
+/*   Updated: 2019/06/03 14:25:55 by atitus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	int i;
+	size_t i;
 
 	i = 0;
-	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && (size_t)i < n)
-	{
+	if (n == 0)
+		return (0);
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && i < (n - 1))
 		i++;
-	}
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
