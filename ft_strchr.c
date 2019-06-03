52ce53da028fecfae78c0c6b43349963d7a9de39
Abdussamad TITUS <atitus@c4r10s2.wethinkcode.co.za>
2019-06-03 15:00:57 +0200

    ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: atitus <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 08:39:22 by atitus            #+#    #+#             */
-/*   Updated: 2019/05/23 09:48:37 by atitus           ###   ########.fr       */
+/*   Updated: 2019/06/03 14:55:57 by atitus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,16 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	while (*s != c && *s != '\0')
-		s++;
-	if (*s == c)
-		return ((char *)s);
-	return (NULL);
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return ((char*)s + i);
+		++i;
+	}
+	if (c != '\0')
+		return (NULL);
+	return ((char*)s + i);
 }
