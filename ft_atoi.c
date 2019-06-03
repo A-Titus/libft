77f5233ae7de1822b11a9cc3f22e89955506f4a9
Abdussamad TITUS <atitus@c4r10s2.wethinkcode.co.za>
2019-06-03 14:44:15 +0200

    ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,32 +6,34 @@
 /*   By: atitus <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 12:23:09 by atitus            #+#    #+#             */
-/*   Updated: 2019/05/31 09:10:13 by atitus           ###   ########.fr       */
+/*   Updated: 2019/06/03 14:43:38 by atitus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_atoi(char *str)
+int		ft_atoi( char *str)
 {
-	int sign;
+	int neg;
 	int i;
-	int res;
+	int num;
 
-	sign = 1;
 	i = 0;
-	res = 0;
-	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '\v' || str[i] == '\r' || str[i] == '\f')
+	neg = 1;
+	num = 0;
+	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '\t'
+			|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
 		i++;
-		while (str[i] == '-')
-		{
-			sign *= -1;
-			i++;
-		}
-		while (str[i] >= '0' && str[i] <= '9')
-		{
-			res = res * 10 + (str[i] - 48);
-			i++;
-		}
-		return (sign * res);
-}	
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			neg *= -1;
+		i++;
+	}
+	while (str[i] >= 48 && str[i] <= 57)
+	{
+		num = num * 10 + (str[i] - 48);
+		i++;
+	}
+	return (num * neg);
+}
