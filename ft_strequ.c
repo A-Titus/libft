bc7ae6b15c98568dd79832e2340dc3248805b44a
Abdussamad TITUS <atitus@c4r10s2.wethinkcode.co.za>
2019-06-03 12:55:20 +0200

    ft_strequ.c

--- ft_strequ.c
+++ ft_strequ.c
@@ -6,7 +6,7 @@
 /*   By: atitus <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/31 09:43:01 by atitus            #+#    #+#             */
-/*   Updated: 2019/05/31 10:02:36 by atitus           ###   ########.fr       */
+/*   Updated: 2019/06/03 12:52:22 by atitus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,7 @@
 int		ft_strequ(char const *s1, char const *s2)
 {
 	if (s1 && s2)
-		if(!ft_strcmp(s1, s2))
+		if (!ft_strcmp(s1, s2))
 			return (1);
 	return (0);
-	
 }
-
-
