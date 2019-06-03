cdecb184b20d55bf53604160eac450849bbb91bb
Abdussamad TITUS <atitus@c4r10s2.wethinkcode.co.za>
2019-06-03 12:58:19 +0200

    ft_striter.c

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: atitus <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/30 09:09:31 by atitus            #+#    #+#             */
-/*   Updated: 2019/05/30 12:05:12 by atitus           ###   ########.fr       */
+/*   Updated: 2019/06/03 12:51:10 by atitus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,10 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 	int i;
 
 	i = 0;
-	while(*s)
+	while (*s)
 	{
 		(*f)(i, s);
 		i++;
 		s++;
-
 	}
 }
