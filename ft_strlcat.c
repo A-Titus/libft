49f5a0b2d1efc5bb94c9ea06541cf24bf26c4141
Abdussamad TITUS <atitus@c4r10s2.wethinkcode.co.za>
2019-06-12 08:57:35 +0200

    ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: atitus <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 10:44:16 by atitus            #+#    #+#             */
-/*   Updated: 2019/06/12 08:30:17 by atitus           ###   ########.fr       */
+/*   Updated: 2019/06/12 08:52:02 by atitus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,19 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t		i;
-	size_t		len;
+	size_t i;
+	size_t j;
 
-	len = ft_strlen(dst) + ft_strlen(src);
-	if (size <= ft_strlen(dst))
-		return (ft_strlen(src) + size);
-	while (*dst)
-		dst++;
 	i = 0;
-	while ((i < size - (len - ft_strlen(src)) - 1) && src[i])
+	j = 0;
+	while (dst[j] && j < size)
+		j++;
+	while (src[i] && (i + j + 1) < size)
 	{
-		dst[i] = src[i];
+		dst[j + i] = src[i];
 		i++;
 	}
-	dst[i] = '\0';
-	return (len);
+	if (i + j < size)
+		dst[i + j] = '\0';
+	return (j + ft_strlen(src));
 }
